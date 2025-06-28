40233225726f6be94d3e7ed648ef70ef9e3b92bc
Shyam0709 <shyam795sundar@gmail.com>
2025-06-28 10:49:10 +0530

    Updated api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://hyperlocal-dmas.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
